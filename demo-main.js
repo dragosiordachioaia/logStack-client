068be8219aa2b688b1ab2b9be976d808a5811656
dragosiordachioaia <xdragosy@gmail.com>
2018-07-25 08:09:38 +0100

    WIP on the library API

--- demo-main.js
+++ demo-main.js
@@ -1,5 +1,7 @@
-document.querySelector("button").addEventListener("click", function(e) {
-  // e.bla();
-  throw new Error();
-  // return err.stack;
+document.querySelector("#button-error").addEventListener("click", function(e) {
+  throw new Error("This is supposed to crash");
+});
+
+document.querySelector("#button-manual").addEventListener("click", function(e) {
+  
 });
--- logstack-client.js
+++ logstack-client.js
@@ -1,29 +1,63 @@
-window.addEventListener("error", onError);
+window.logStack = (function() {
+  window.addEventListener("error", onUncaughtError);
 
-function onError(event) {
-  console.warn(event);
-  console.warn(event.stack);
-  let stack;
-  if (event.error && event.error.stack) {
-    stack = event.error.stack;
+  function report(params) {
+
   }
-  let errorJSON = {
-    colno: event.colno,
-    lineno: event.lineno,
-    message: event.message,
-    filename: event.filename,
-    timeStamp: event.timeStamp,
-    type: event.type,
-    stack: stack,
-  };
 
-  console.log("to report:");
-  console.log(errorJSON);
-  // console.log("something");
+  function getNavigatorProps() {
+    let navigatorData = {};
+    for (let prop in window.navigator) {
+      if(
+        typeof (window.navigator[prop]) !== "object" &&
+        typeof (window.navigator[prop]) !== "function"
+      ) {
+        navigatorData[prop] = window.navigator[prop];
+      }
+    }
+    navigatorData["languages"] = window.navigator.languages;
+    return navigatorData;
+  }
 
-  axios({
-    method: "POST",
-    url: "http://localhost:8080/api/issues",
-    data: errorJSON,
-  });
-}
+  function onUncaughtError(event) {
+    console.warn(event);
+    console.warn(event.stack);
+    let stack;
+
+    if (event.error && event.error.stack) {
+      stack = event.error.stack;
+    }
+    let errorData = {
+      colno: event.colno,
+      lineno: event.lineno,
+      message: event.message,
+      filename: event.filename,
+      timeStamp: event.timeStamp,
+      type: "error",
+      stack: stack,
+    };
+
+    console.log("to report:");
+    console.log(errorJSON);
+
+    makeCall(errorData);
+  }
+
+  function makeCall(data) {
+    const LOGSTACK_BASE_URL = "http://localhost:8080/api";
+    const LOGSTACK_API_VERSION = "v1";
+    const LOGSTACK_ENDPOINT = "issues"
+
+    data.navigator = getNavigatorProps();
+
+    axios({
+      method: "POST",
+      url: `${LOGSTACK_BASE_URL}/${LOGSTACK_API_VERSION}/${LOGSTACK_ENDPOINT}/`,
+      data: data,
+    });
+  }
+
+  return {
+    report: report,
+  }
+})();
